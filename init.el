a39dac612bbda7f8e7e7017e064142b823794fec
Mike Miller <mike@6502studios.com>
2019-11-14 16:15:43 -0500

    just start at runlevel 5 all the time

--- init.el
+++ init.el
@@ -1,12 +1,7 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (load "base")
 (load-file-if-exists "~/.personal.el")
-(startup-emacs
- (cond
-  ((and window-system
-	(string-equal "luna" system-name))
-   5)
-  (window-system 4)))
+(startup-emacs 5)
 (load "plat")
 (load "window")
 (load-file-if-exists custom-file 'noerror)
