7a61562d7590ab545d001df44ce92ef1f7b708cb
Mike Miller <mike@6502studios.com>
2019-11-14 16:15:24 -0500

    updated custimizations

--- custom.el
+++ custom.el
@@ -3,37 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(blink-cursor-mode nil)
- '(fringe-mode 4 nil (fringe))
- '(menu-bar-mode nil)
- '(package-selected-packages
-   (quote
-    (speed-type dired-du oauth2 slack queue htmlize csv-mode clojure-mode adaptive-wrap)))
- '(safe-local-variable-values (quote ((checkdoc-package-keywords-flag))))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 25)
- '(tool-bar-mode nil)
- '(window-divider-default-bottom-width 0)
- '(window-divider-default-places t)
- '(window-divider-default-right-width 1)
- '(window-divider-mode nil))
+ '(display-time-mode t)
+ '(indicate-empty-lines t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Info-quoted ((t (:inherit nil :weight bold))))
- '(erc-nick-default-face ((t (:foreground "blue" :weight bold))))
- '(erc-timestamp-face ((t (:foreground "blue" :weight bold))))
- '(jabber-chat-prompt-foreign ((t (:weight bold))))
- '(jabber-chat-prompt-local ((t (:foreground "blue"))))
- '(jabber-chat-prompt-system ((t (:foreground "red"))))
- '(jabber-title-medium ((t (:height 1.5))))
- '(magit-tag ((t (:background "yellow" :foreground "black"))))
- '(mu4e-header-highlight-face ((t (:background "grey" :underline t :weight bold))))
- '(mu4e-unread-face ((t (:inherit font-lock-keyword-face :foreground "blue" :weight bold)))))
+ )
 (put 'narrow-to-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
