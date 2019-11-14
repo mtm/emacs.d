7fa199d077a6e73760f7a13aee463ae057a0907e
Mike Miller <mike@6502studios.com>
2019-11-14 16:43:11 -0500

    more customizations

--- custom.el
+++ custom.el
@@ -3,10 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(cider-auto-select-error-buffer nil)
+ '(cider-special-mode-truncate-lines nil)
+ '(display-battery-mode t)
  '(display-time-mode t)
+ '(fringe-mode (quote (nil . 0)) nil (fringe))
  '(indicate-empty-lines t)
+ '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
